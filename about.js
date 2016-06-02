6304771b55870ef02334c696c89aea6b9575fd2e
Geoffrey Churchill <geoffrey.a.churchill@gmail.com>
2016-06-02 12:07:58 -0400

    allow enter key on about.html

--- about.js
+++ about.js
@@ -1,6 +1,9 @@
 function submitForm(formData){ // just ignore response
     var xhr = new XMLHttpRequest();
-    //xhr.onload=function(e){console.log('ajax returned', e);};
+    xhr.onload=function(e){
+	console.log('ajax returned', e);
+	document.getElementById('contactForm').reset();
+    };
     xhr.open('POST', 'https://docs.google.com/forms/d/1Rg_XyGY3KDP-xVNVFEGcQVRfzuoYzNJFNeRyU35P4wU/formResponse?embedded=true', true);
     xhr.send(formData);
 }
@@ -8,4 +11,11 @@ function submitForm(formData){ // just ignore response
 window.onload=function(){
     initGrid(document.body);
     passAlongEvents(document.getElementById('mainDiv'));
+    document.getElementById('emailInput').onkeydown=function(e){
+	if(e.keyCode==13){
+	    e.preventDefault();
+	    document.getElementById('submitLabel').onclick();
+	    return false;
+	}
+    };
 };   

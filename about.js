d6f72ee566f5c63a77358410138bbef401cc9369
Geoffrey Churchill <geoffrey.a.churchill@gmail.com>
2016-06-02 12:27:03 -0400

    cross-origin not allowed. revert

--- about.js
+++ about.js
@@ -1,11 +1,11 @@
 function submitForm(formData){ // just ignore response
-    var xhr = new XMLHttpRequest();
+    /*var xhr = new XMLHttpRequest();
     xhr.onload=function(e){
 	console.log('ajax returned', e);
 	document.getElementById('contactForm').reset();
     };
     xhr.open('POST', 'https://docs.google.com/forms/d/1Rg_XyGY3KDP-xVNVFEGcQVRfzuoYzNJFNeRyU35P4wU/formResponse?embedded=true', true);
-    xhr.send(formData);
+    xhr.send(formData);*/
 }
 
 window.onload=function(){
@@ -14,12 +14,13 @@ window.onload=function(){
     /*document.getElementById('contactForm').onsubmit=function(e){
 	//e.preventDefault();
 	return false;
-    };*/
+	};*/
+    /*
     document.getElementById('emailInput').onkeydown=function(e){
 	if(e.keyCode==13){
 	    e.preventDefault();
 	    document.getElementById('submitLabel').onclick();
 	    return false;
 	}
-    };
+    };*/
 };   
